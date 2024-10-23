6fc0f64f439f34a39ee1ad315a2c63b0d5466773
Ayushraj021 <ayushraj02929@gmail.com>
2024-10-23 17:15:47 +0530

    Assigment-5

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ region = "ap-south-1"
 resource "aws_instance" "one" {
 ami = "ami-0e0e417dfa2028266"
 instance_type = "t2.micro"
+vpc_security_group_ids = ["sg-0b20ea9c881f8aade"]
 tags={
     Name = "Jenkins"
 }
